f0025359018836f5d5e8ccea2b2a8bfd12a98610
ashishpapanai <ashishpapanai00@gmail.com>
2023-03-18 01:00:38 +0530

    Fixed: Audio APIs

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 # dummy flask app
 import os
+import requests
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 from profanity_check import predict, predict_prob
 from audio import stt
 import tempfile
+import audiofile as af
+import random
 
 
 app = Flask(__name__)
@@ -14,17 +17,23 @@ CORS(app)
 def hello_world():
     return render_template('index.html')
 
-@app.route('/audio/<string:url>', methods=['POST'])
-def audioObscenity(url):
-    # check if url is valid
-    if not url.startswith('https://') or not url.startswith('http://'):
-        return jsonify({'error': 'invalid url'})
-    
-    tmp = tempfile.NamedTemporaryFile()
-    tmp.write(requests.get(url).content)
-    text = stt.main(tmp.name)
-    tmp.close()
-    return jsonify({'prediction': str(pred[0]), 'probability': str(probability[0])})   
+@app.route('/audio/')
+def audioObscenity():
+    import time
+    start = time.time()
+    audio = request.args.get('url')
+    if audio is None:
+        return jsonify({'error': 'no audio url'})
+    with tempfile.NamedTemporaryFile(suffix='.wav') as tmp:
+        os.system(f"wget {audio} -O {tmp.name}")
+        audio, sample_rate = af.read(tmp.name)
+        # save as wav with 16k bitrate and 30 random seconds
+        start_index = random.randint(0, len(audio) - 480000)
+        af.write(tmp.name, audio[start_index:start_index+480000], sample_rate)
+        text = stt.main(tmp.name)
+        print(text)
+        print(time.time() - start)
+        return textObscenity(text)
 
 @app.route('/text/<string:input_string>')
 def textObscenity(input_string):
